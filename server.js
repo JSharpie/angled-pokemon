93533ff5e3fc1dd81f8b85cdd9e570d65530c9ec
Jacob Sharp <sidewayseightgmod@gmail.com>
2015-12-02 14:34:19 -0500

    fixing bower

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res){
   res.sendFile('app');
 });
-app.get('/maybe', function(req, res){
-  res.send('<p>what am I even doing</p>');
-});
 
 http.listen(port);
 console.log('WE ARE RUNNING ON PORT ' + port);
